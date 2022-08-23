6719dacceca5a2af76ca38946f0e4d8e38c2e04f
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-08-23 08:51:22 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+ let form= document.querySelector('form')
+ form.addEventListener('submit', (e)=> {
+  e.preventDefault()
+  buildEvents(e.target.newtaskdescription.value)
+  form.reset()
+ })
 });
