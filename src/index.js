4226312fdefaf28b2313616a16d4d7b2d3b7d927
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-09-05 15:41:19 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
- let form= document.querySelector('form')
- form.addEventListener('submit', (e)=> {
-  e.preventDefault()
-  buildEvents(e.target.newtaskdescription.value)
-  form.reset()
- })
+  submitTask()
 });
+// Handles an event in a form based on clicking a submit button.
+// Handles an event in a form based on clicking a submit button.
+let submitTask = () => {
+	let taskList = document.getElementById('tasks');
+	let taskEntry = document.getElementById('create-task-form');
+	taskEntry.onsubmit = (e) => {
+		e.preventDefault(); 
+		let newTaskDescription = document.getElementById('new-task-description').value;
+		var entry = document.createElement('li');
+		entry.appendChild(document.createTextNode(newTaskDescription));
+		taskList.appendChild(entry);
+		taskEntry.reset()
+	}
+}
